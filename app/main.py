2558d61ee42dc25f6589ad2941f53cd6f74d1032
Felipe Tomazelli <fefetl08@gmail.com>
2021-08-15 23:43:11 -0300

    validation working with dynamic ids

---
+++ app/main.py
@@ -0,0 +1,10 @@
+﻿from typing import Dict, Optional
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+from models.models import AllPlayers 
+
+app = FastAPI()
+
+@app.post("/")
+def root(allplayers: Dict[str, AllPlayers]):
+    return allplayers
