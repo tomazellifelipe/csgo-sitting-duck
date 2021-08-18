22d94058ec3543f2c953ec06801e56af1aa6c776
Felipe Tomazelli <fefetl08@gmail.com>
2021-08-18 13:13:23 -0300

    fixed unvalid data types

--- app/models/misc_models.py
+++ app/models/misc_models.py
@@ -1,14 +1,26 @@
 from typing import Dict, Optional
+
 from pydantic import BaseModel, Field
-from models.player_models import Player
+
 from models.map_models import Map
-
-
-class Previously(BaseModel):
-    allplayers: Optional[Dict[str, Player]] = None
-    map_info: Optional[Map] = Field(None, alias='map')
+from models.player_models import Player
+from models.server_models import Bomb, PhaseCountdowns, Round
 
 
 class Added(BaseModel):
     allplayers: Optional[Dict[str, Player]] = None
+    bomb: Optional[Bomb] = None
     map_info: Optional[Map] = Field(None, alias='map')
+    player: Optional[Player] = None
+    phase_countdowns: Optional[PhaseCountdowns] = None
+    round_phase: Optional[Round] = Field(None, alias='round')
+
+
+class Previously(BaseModel):
+    allplayers: Optional[Dict[str, Player]] = None
+    bomb: Optional[Bomb] = None
+    map_info: Optional[Map] = Field(None, alias='map')
+    player: Optional[Player] = None
+    phase_countdowns: Optional[PhaseCountdowns] = None
+    round_phase: Optional[Round] = Field(None, alias='round')
+
