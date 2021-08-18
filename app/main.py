5f16522ff4297b92278ce2a6dff045dd74e6d9bb
Felipe Tomazelli <fefetl08@gmail.com>
2021-08-18 13:14:09 -0300

    added new endpoints and body validation

--- app/main.py
+++ app/main.py
@@ -1,15 +1,31 @@
 ﻿from typing import Dict, Optional
+
 from fastapi import Body, FastAPI
-from models.player_models import Player
+
 from models.map_models import Map
-from models.misc_models import Previously, Added
+from models.misc_models import Added, Previously 
+from models.player_models import Player
+from models.server_models import Bomb, PhaseCountdowns, Provider, Round
 
 
 app = FastAPI()
 
-@app.post("/")
-def root(map_info: Optional[Map] = Body(None, alias='map', embed=True), 
-         allplayers: Optional[Dict[str, Player]] = Body(None, embed=True),
+@app.post("/observer")
+def observer(allplayers: Optional[Dict[str, Player]] = Body(None, embed=True),
+             bomb: Optional[Bomb] = Body(None, embed=True),
+             map_info: Optional[Map] = Body(None, alias='map', embed=True), 
+             phase_countdowns: Optional[PhaseCountdowns] = Body(None, embed=True),
+             provider: Optional[Provider] = Body(None, embed=True),
+             round_phase: Optional[Round] = Body(None, alias='round', embed=True),
+             previously: Optional[Previously] = Body(None, embed=True),
+             added: Optional[Added] = Body(None, embed=True)):
+    return
+
+@app.post("/live")
+def live(map_info: Optional[Map] = Body(None, alias='map', embed=True), 
+         player: Optional[Player] = Body(None, embed=True),
+         provider: Optional[Provider] = Body(None, embed=True),
+         round_phase: Optional[Round] = Body(None, alias='round', embed=True),
          previously: Optional[Previously] = Body(None, embed=True),
          added: Optional[Added] = Body(None, embed=True)):
     return
