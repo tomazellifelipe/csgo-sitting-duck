99a7c54abf9c1977ae67137774fa53064e4e57b6
Felipe Tomazelli <fefetl08@gmail.com>
2021-08-19 11:56:35 -0300

    testing dynamic models based on user input

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 ﻿from fastapi import Body, FastAPI
+from pydantic import create_model
 
 from models.payload_models import Payload
+
+DynamicModel = create_model('DynamicModel',
+                            fruit='banana',
+                            __base__=Payload)
 
 app = FastAPI()
 
 @app.post("/observer")
-def observer(payload: Payload = None):
+def observer(payload: DynamicModel = None):
     return payload
 
