b237ee48400976d67385f58c8492a38d23731c26
Felipe Tomazelli <fefetl08@gmail.com>
2021-08-21 16:20:49 -0300

    removed the dynamic field

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
 ﻿from fastapi import Body, FastAPI
-from pydantic import create_model
 
 from models.payload_models import Payload
 
-DynamicModel = create_model('DynamicModel',
-                            fruit='banana',
-                            __base__=Payload)
 
 app = FastAPI()
 
 @app.post("/observer")
-def observer(payload: DynamicModel = None):
+def observer(payload: Payload = None):
     return payload
 
